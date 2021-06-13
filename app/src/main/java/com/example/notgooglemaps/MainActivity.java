a95e5297dab46f57221e10d40b0df6da0a6de1e7
epiKtry <saucysushi19@gmail.com>
2021-06-12 20:53:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/notgooglemaps/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.notgooglemaps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button start = (Button)findViewById(R.id.button);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent move = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(move);
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/com/example/notgooglemaps/MapsActivity.java
@@ -0,0 +1,55 @@
+package com.example.notgooglemaps;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.notgooglemaps.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng myHouse = new LatLng(32.64791791913678, -85.52119620109112);
+        mMap.addMarker(new MarkerOptions().position(myHouse).title("Shin's House"));
+
+        LatLng samfordTennisCenter = new LatLng(32.59955452177241, -85.46244059870233);
+        mMap.addMarker(new MarkerOptions().position(samfordTennisCenter).title("Samford Tennis Courts"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(samfordTennisCenter, 15));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myHouse, 15));
+    }
+}
